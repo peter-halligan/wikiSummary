7c5cbe0ca5cbbc46e830ccfa27fe0c4e3f5982bb
peter-halligan <peter.halligan@mycit.ie>
2020-06-18 14:41:19 +0000

    feat: add logic to function

--- wikiSummary/lambda_function.py
+++ wikiSummary/lambda_function.py
@@ -1,3 +1,22 @@
+import json
+import wikipedia
+
+
 def lambda_handler(event, context):
     # TODO implement
-    return ''
+    if 'body' in event:
+        body = json.loads(event['body'])
+        
+    entity = body['entity']
+    res = wikipedia.summary(entity, sentence=1)
+    
+    print(f"context: {context}, event: {event}")
+    print(f"Response from wikipedia API: {res}")
+    
+    response = {
+        "statusCode": "200",
+        "headers": { "Content-type": "application/json" },
+        "body": json.dumps({"res": res})
+    }
+
+    return response
