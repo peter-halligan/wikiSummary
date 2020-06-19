842871c2c33491d7ee2c68d6c0f73c0fcde2b37d
peter <pha@tiatechnology.com>
2020-06-19 08:08:04 +0200

    feat: return 400 if no entity passed

--- wikiSummary/lambda_function.py
+++ wikiSummary/lambda_function.py
@@ -3,20 +3,26 @@ import wikipedia
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    if 'body' in event:
-        body = json.loads(event['body'])
+
         
-    entity = body['entity']
-    res = wikipedia.summary(entity, sentence=1)
+    if 'entity' in event:
+        entity = event['entity']
+        
+        res = wikipedia.summary(entity, sentences=1)
+
+        print(f"context: {context}, event: {event}")
+        print(f"Response from wikipedia API: {res}")
     
-    print(f"context: {context}, event: {event}")
-    print(f"Response from wikipedia API: {res}")
-    
-    response = {
-        "statusCode": "200",
-        "headers": { "Content-type": "application/json" },
-        "body": json.dumps({"res": res})
-    }
+        response = {
+            "statusCode": "200",
+            "headers": { "Content-type": "application/json" },
+            "body": json.dumps({"res": res})
+        }
+    else:
+        response = {
+            "statusCode": "400",
+            "body": []
+        }
+
 
     return response
